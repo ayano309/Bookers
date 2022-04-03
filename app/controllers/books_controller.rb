852ef603b676dea89477d77ce732cb48df46a998
ayano309 <ayatomato.k@gmail.com>
2022-04-03 12:51:24 +0000

    [update]indexページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
     def index
         @books = Book.all
+        @book=Book.new
     end
     
     def show
@@ -8,12 +9,20 @@ class BooksController < ApplicationController
     end
     
     def new
-        
+        @book = Book.new
     end
     
     def create
-        
-    end
+        @book = Book.new(book_params)
+    
+        if @book.save
+          flash[:notice] = "Book was successfully created."
+          redirect_to book_path(@book)
+        else
+          @books = Book.all
+          render :index
+        end
+  end
     
     def edit
     
@@ -25,6 +34,13 @@ class BooksController < ApplicationController
     end
     
     def destroy
+        
+    end
+   
+    
+    private
+    def book_params
+        params.require(:book).permit(:title, :body)
         
     end
 end
